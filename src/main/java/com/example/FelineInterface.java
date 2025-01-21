61a935ac0e31e411cc280ffacb24123fe8791695
Antonina Mazurova <out-cold@yandex.ru>
2025-01-21 11:20:35 +0300

    fixed Lion from Feline isolation

--- src/main/java/com/example/FelineInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import java.util.List;
-
-public interface FelineInterface {
-    int getKittens();
-    String getFamily();
-    List<String> getFood(String type) throws Exception;
-}
