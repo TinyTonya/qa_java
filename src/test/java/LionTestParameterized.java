f5f820e899e991990194fa0361978837a029c9d5
Antonina Mazurova <out-cold@yandex.ru>
2025-01-21 11:45:23 +0300

    deleted extends LionTest

--- src/test/java/LionTestParameterized.java
+++ src/test/java/LionTestParameterized.java
@@ -1,33 +1,47 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-public class LionTestParameterized extends LionTest {
+@RunWith(Parameterized.class)
+public class LionTestParameterized {
 
-    public LionTestParameterized() {}
+        @Mock
+        private Feline feline;
 
-    @Parameterized.Parameters
-    public static Object[][] data() {
-        return new Object[][]{
-                {5, 5},
-                {1, 1},
-                {10, 10}
-        };
+        private Lion lion;
+
+        @Before
+        public void init () throws Exception {
+            MockitoAnnotations.initMocks(this);
+            lion = new Lion("Самец", feline);
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] data () {
+            return new Object[][]{
+                    {5, 5},
+                    {1, 1},
+                    {10, 10}
+            };
+        }
+
+        @Parameterized.Parameter(0)
+        public int kittensCount;
+
+        @Parameterized.Parameter(1)
+        public int expectedKittensCount;
+
+        @Test
+        public void testGetKittensWithParameter () throws Exception {
+            when(feline.getKittens()).thenReturn(kittensCount);
+            assertEquals(expectedKittensCount, lion.getKittens());
+        }
     }
-
-    @Parameterized.Parameter(0)
-    public int kittensCount;
-
-    @Parameterized.Parameter(1)
-    public int expectedKittensCount;
-
-    @Test
-    public void testGetKittensWithParameter() throws Exception {
-        when(feline.getKittens()).thenReturn(kittensCount);
-        assertEquals(expectedKittensCount, lion.getKittens());
-    }
-}
